bdbb97ac959fd7e516696a9b3c61e5ad38c25c10
Arda "Ave" Ozkal <ave@ave.zone>
2018-11-07 11:23:21 +0300

    source: tiny bugfix

--- cogs/basic.py
+++ cogs/basic.py
@@ -16,7 +16,7 @@ class Basic:
     async def source(self, ctx):
         """Gives link to source code."""
         await ctx.send("You can find my source at " +
-                       self.bot.config['base']['guild'] +
+                       self.bot.config['base']['source'] +
                        ". Serious PRs and issues welcome!")
 
     @commands.command(aliases=['p'])
