41533658d6c57b8ee713af345d8472f833188dc2
Arda "Ave" Ozkal <ave@ave.zone>
2018-11-07 12:32:18 +0300

    Add moderation cog

---
+++ cogs/moderation.py
@@ -0,0 +1,23 @@
+from discord.ext import commands
+import asyncio
+import config
+
+
+class Moderation:
+    def __init__(self, bot):
+        self.bot = bot
+        self.config = bot.config
+
+    def check_if_staff(self, ctx):
+        return any(r.id in config.staff_role_ids for r in ctx.author.roles)
+
+    @commands.check(check_if_staff)
+    @commands.command()
+    async def purge(self, ctx, count: int):
+        """Purges a number of messages, staff only."""
+        count = 100 if count > 100 else count
+        ctx.channel.purge(count)
+
+
+def setup(bot):
+    bot.add_cog(Moderation(bot))
